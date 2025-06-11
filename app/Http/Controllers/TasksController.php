8894c32779e45dbcbcafbb7a59ab86866b40fbe8
K.K <Kakumori.Koki@trans-cosmos.co.jp>
2025-06-11 17:02:40 +0900

    make basic views

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -11,7 +11,7 @@ class TasksController extends Controller
      */
     public function index()
     {
-        //
+        return view('tasks.index');
     }
 
     /**
---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,7 @@
+<header>
+  <div class="navbar bg-base-100 shadow-sm mb-4">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">TaskList</a>
+    </div>
+  </div>
+</header>
